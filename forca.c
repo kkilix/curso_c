1800d389efe8861c17ddbfb005f8e024d5686aeb
Carlos Antonio da Silva <kkilix@hotmail.com>
2016-12-26 14:09:38 -0200

    Jogo da forca parte 1

---
+++ forca.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	char palavrasecreta[20];
+	sprintf(palavrasecreta, "MELANCIA");
+
+	int acertou = 0;
+	int enforcou = 0;
+
+	do {
+		char chute;
+		
+		printf("Qual letra? ");
+		scanf(" %c", &chute);
+
+        int i = 0;
+
+		for(; i < strlen(palavrasecreta); i++) {
+			if(palavrasecreta[i] == chute) {
+				printf("A posição %d tem essa letra\n", i+1);
+			}
+		}
+	} while (!acertou && !enforcou);
+
+}
