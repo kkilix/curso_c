d0fe9fa0682f7294073db984ecf5ed8dc79a9554
Carlos Antonio da Silva <kkilix@hotmail.com>
2016-12-27 14:20:46 -0200

    Criando o header file forca.h

--- curso-2/forca/forca.c
+++ curso-2/forca/forca.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "forca.h"
 
 // variáveis globais
 char palavrasecreta[20];
---
+++ curso-2/forca/forca.h
@@ -0,0 +1,8 @@
+// lista de funções que aparecerão no arquivo
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
