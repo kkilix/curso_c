680d1eeedf631e027082947d7818c6cb6810c73e
Carlos Antonio da Silva <kkilix@hotmail.com>
2017-01-02 11:40:42 -0200

    Criando matriz dinamicamente

--- curso-3/fogefoge/fogefoge.c
+++ curso-3/fogefoge/fogefoge.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
-int main() {
+// variáveis globais
+char** mapa;
+int linhas;
+int colunas;
+int i;
 
+void lemapa() {
 	FILE* f;
-	char mapa[5][10+1];
-	int i;
-
 	f = fopen("mapa.txt", "r");
 	if(f == 0) {
 		printf("Erro na leitura do mapa");
 		exit(1);
 	}
 
+	fscanf(f, "%d %d", &linhas, &colunas);
+	alocamapa();
+	
 	for(i = 0; i < 5; i++) {
 		fscanf(f, "%s", mapa[i]);
 	}
 
-	for(i = 0; i < 5; i++) {
+	fclose(f);
+}
+
+void alocamapa() {
+	mapa = malloc(sizeof(char*) * linhas);
+
+	for(i = 0; i < linhas; i++) {
+		mapa[i] = malloc(sizeof(char) * colunas + 1);
+	}
+}
+
+void liberamapa() {	
+	for(i = 0; i < linhas; i++) {
+		free(mapa[i]);
+	}
+
+	free(mapa);
+}
+
+
+int main() {
+
+	lemapa();
+
+	for(i = 0; i < linhas; i++) {
 		printf("%s\n", mapa[i]);
 	}
 
-	fclose(f);
+	liberamapa();
 
 }
