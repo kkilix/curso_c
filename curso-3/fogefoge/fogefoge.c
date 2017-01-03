f9f1e6c9d7960c53718e5fcdee096dd7d2cbcc22
Carlos Antonio da Silva <kkilix@hotmail.com>
2017-01-03 11:17:26 -0200

    Refatorando a função move em 3 novas funções

--- curso-3/fogefoge/fogefoge.c
+++ curso-3/fogefoge/fogefoge.c
@@ -14,13 +14,17 @@ int acabou() {
 	return 0;
 }
 
+int ehDirecao(char direcao) {
+	return
+		direcao == 'a' || 
+		direcao == 'w' ||
+		direcao == 's' ||
+		direcao == 'd';
+}
+
 void move(char direcao) {
-	if(
-		direcao != 'a' && 
-		direcao != 'w' &&
-		direcao != 's' &&
-		direcao != 'd'
-	) return;
+	if(!ehDirecao(direcao))	
+		return;
 
 	int proximox = heroi.x;
 	int proximoy = heroi.y;
@@ -40,15 +44,13 @@ void move(char direcao) {
 			break;
 	}
 
-	if(proximox >= m.linhas) 
-		return;
-	if(proximoy >= m.colunas) 
-		return;
-	if(m.matriz[proximox][proximoy] != '.') 
+	if(!ehValida(&m, proximox, proximoy))
 		return;
 
-	m.matriz[proximox][proximoy] = '@';
-	m.matriz[heroi.x][heroi.y] = '.';
+	if(!ehVazia(&m, proximox, proximoy))
+		return;
+
+	andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
 	heroi.x = proximox;
 	heroi.y = proximoy;
 }
